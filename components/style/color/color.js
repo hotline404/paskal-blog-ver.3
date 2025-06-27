ac1bbb505bd1feadd66fc182cf983c2d2f704a00
hotline404 <cosmoyj7733@gmail.com>
2025-06-27 16:47:05 +0900

    feat: 스타일 리터럴 폴더 추가

---
+++ components/style/color/color.js
@@ -0,0 +1,26 @@
+export const color = {
+  background : {
+    base_note: "bg-[#5AD060]",
+    modal_note: "bg-[#868686]",
+  },
+
+  pageColor: {
+    base_note: "bg-[#FAF4E6]",
+    modal_note: "bg-[#FFFFFF]"
+  },
+
+  outline: {
+    base_note: "outline-[#31302B]",
+    modal_note: "outline-[#000000]"
+  },
+
+  fontColor: {
+    base_note: "text-[#31302B]",
+    modal_note: "text-[#3934D8]"
+  },
+
+  shadowColor: {
+    base_note: "shadow-(color: #31302B)",
+    modal_note: "none"
+  }
+}
---
+++ components/style/size/size.js
@@ -0,0 +1,42 @@
+export const size = {
+  width: {
+    background: "",
+    page: "",
+    button: "",
+    input: "",
+    tag: "",
+    titleBox: "",
+    modal_background: "",
+    modal_page: "",
+    modal_titleBox: "",
+    modal_aticleBox: "",
+    escButton: ""
+  },
+
+  height: {
+    background: "",
+    page: "",
+    button: "",
+    input: "",
+    tag: "",
+    titleBox: "",
+    modal_background: "",
+    modal_page: "",
+    modal_titleBox: "",
+    modal_aticleBox: "",
+    escButton : ""
+  },
+
+  font: {
+    title: "",
+    input: "",
+    tag: "",
+    postLists: "",
+    modal_title: "",
+    modal_date: "",
+    modal_aticle: ""
+  },
+};
+
+
+// rounded-[] 는 mixed에서 인자에 따라 달라지게 할 것 [6, 10, 28], 
