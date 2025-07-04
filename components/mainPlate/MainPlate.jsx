ff2d7003639e57bcc1bdc2d161cf281cbbcda978
hotline404 <cosmoyj7733@gmail.com>
2025-07-04 20:41:04 +0900

    feat: 포스트 리스트 컴포넌트 추가

--- components/mainPlate/MainPlate.jsx
+++ components/mainPlate/MainPlate.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import PostList from './PostList'
+
+const data = [
+  {
+    ha: "afdfsf",
+    af: "asdsf",
+    asd: "1234"
+  },
+  {
+    ha: "afd4sf",
+    af: "asdgf",
+    asd: "12345"
+  },
+  {
+    ha: "afd5sf",
+    af: "asdjf",
+    asd: "12346"
+  },
+]
 
 function MainPlate() {
   return (
     <div>
       MainPlaite vamos
-      {}
+      <PostList data={data}/>
     </div>
   )
 }
