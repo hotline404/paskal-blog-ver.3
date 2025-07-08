3a5fc061eef857afb723213bc402d878b3af65a8
hotline404 <cosmoyj7733@gmail.com>
2025-07-08 15:58:12 +0900

    feat: 리스트 컴포넌트 추가

---
+++ components/common/List.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+//리스트 캡슐화하기
+
+function List(props) {
+  return (
+    <ol>
+      {props.lists.map((list) => {
+        return (
+          <li>
+            <h1>{list.title}</h1>
+            <p>{list.date}</p>
+          </li>
+        )
+      })}
+    </ol>
+  )
+}
+
+export default List;
