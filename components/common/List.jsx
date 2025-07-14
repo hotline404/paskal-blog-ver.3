4f1dc36fa69fdc2c99cb297d54f058c1b6af3874
hotline404 <cosmoyj7733@gmail.com>
2025-07-14 15:18:11 +0900

    feat: list style

--- components/common/List.jsx
+++ components/common/List.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from "react";
 //리스트 캡슐화하기
 
 function List(props) {
   return (
-    <ol className="flex">
+    <ol className="flex w-[100%] flex-wrap justify-center items-center">
       {props.lists.map((list) => {
         return (
-          <li className="outline-[#31302B] outline-[5px] rounded-[10px] " key={list.post_id}>
-            <h1>{list.title}</h1>
-            <p>{list.date}</p>
+          <li
+            className="outline-[#31302B] outline-[5px] rounded-[10px] w-[calc(90%/3)] mx-[10px] my-[10px] p-[10px]"
+            key={list.post_id}
+          >
+            <h1 className="font-bold">{list.title}</h1>
+            <p className="">{list.date}</p>
           </li>
-        )
+        );
       })}
     </ol>
-  )
+  );
 }
 
 export default List;
