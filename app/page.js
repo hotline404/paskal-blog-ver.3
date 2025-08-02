ac477868131079f6bf4e15a027c1c9988797c8a0
hotline404 <cosmoyj7733@gmail.com>
2025-08-02 19:02:23 +0900

    faet: 정적 최적화

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import { mixtureStyle } from "@/components/style/mixture/mixture";
 import Header from "@/components/header/Header";
 import ListBox from "@/components/box/ListBox";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   const homePageStyle = mixtureStyle;
   return (
