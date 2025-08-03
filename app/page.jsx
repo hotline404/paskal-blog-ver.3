68e96f412c06e13b34e1ffdf7c80d82a8c471f13
hotline404 <cosmoyj7733@gmail.com>
2025-08-03 10:20:57 +0900

    feat: config 수정 및 apge.js 수정

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 // 루트 페이지 크기 및 페이지 색상
+import React from "react";
 import MainBox from "@/components/box/MainBox";
 import { mixtureStyle } from "@/components/style/mixture/mixture";
 import Header from "@/components/header/Header";
@@ -9,7 +10,7 @@ export const metadata = { title: "홈", description: "루트 페이지" };
 
 
 function Home() {
-  console.log("tq tq tq tq")
+
   const homePageStyle = mixtureStyle;
   return (
     <div className={homePageStyle.base_note}>
