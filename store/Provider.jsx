668f9707f4a9b6d46f5c2d96a2d321a1f64741c2
hotline404 <cosmoyj7733@gmail.com>
2025-08-10 16:04:41 +0900

    feat: add useefe

--- store/Provider.jsx
+++ store/Provider.jsx
@@ -1,24 +1,19 @@
 "use client";
 
-import React, { useEffect } from 'react';
-import useStore from './state';
+import React, { useEffect } from "react";
+import useStore from "./state";
 
 function Provider({ children, data }) {
+  const { initialData, initialize } = useStore();
 
+  useEffect(() => {
+    data.then((data) => initialize(data));
+    console.log("initialData", initialData);
+  }, [initialize]);
 
-  const {initialData, initialize} = useStore();
-  data.then(data => initialize(data));
+  console.log("initialData", initialData);
 
-  
-  // useEffect(() => {
-  //   console.log("initialData", initialData);
-  // }, [])
-  
-  return (
-    <div>
-      {children}
-    </div>
-  )
+  return <div>{children}</div>;
 }
 
-export default Provider
+export default Provider;
