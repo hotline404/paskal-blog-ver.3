edc4b11ea354bf784fbb05f3abd822954b18bf63
hotline404 <cosmoyj7733@gmail.com>
2025-08-13 18:19:08 +0900

    del : notino client

--- API/service.js
+++ API/service.js
@@ -1,5 +1,5 @@
 const {notion_key, notion_DB} = require("./instance.js");
-import { NotionAPI } from "notion-client";
+// import { NotionAPI } from "notion-client";
 const { Client } = require("@notionhq/client");
 
 const notion = new Client({
@@ -7,10 +7,10 @@ const notion = new Client({
   request_timeout: 10000,
 });
 
-const notionAPI = new NotionAPI({
-  activeUser: "b8d00329-393d-4390-9f8e-b783e2fe1768",
-  authToken: "v03%3AeyJhbGciOiJkaXIiLCJraWQiOiJwcm9kdWN0aW9uOnRva2VuLXYzOjIwMjQtMTEtMDciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..PbSFRsSH0LhIkZdcgtpn9Q.RTW1xXnlrViPlU1NFWGrXtlQrHupN-eHGvfNIsVBpU8l2kHV8wiqPliKce-HKvxlOIertDmJ87GHz2LUMxrYBZLRLkU1HSRe7aG8xaTumA0oGdSfGWABbk7_t2sP7VzjBf5sRm-isZwVTixT05poHFw1Y4L_w9bnMKExcD1OE5dXg-xgJMTzqChyLaTcWT1Z3PxZzz0V_xapAakJEdDoYLt0UFr9qfmnfAY8a-5S7CV-6yDhXLZplvixnBAyaxBPvXyB7mzzZpPkNWp4nPcFBiI8F7mUBjcL7GJ_MyBQ5qEu4Me80gutoTHfQ0tL_R0i06BTwIuxPflJ9fQ1wnEcdkGFs66xFmzGEWA4dz9WB1U.ANYk0XXT7n_LUxr0kJ9spIuZ5itMcrHLqQIv76pL6mo"
-});
+// const notionAPI = new NotionAPI({
+//   activeUser: "b8d00329-393d-4390-9f8e-b783e2fe1768",
+//   authToken: "v03%3AeyJhbGciOiJkaXIiLCJraWQiOiJwcm9kdWN0aW9uOnRva2VuLXYzOjIwMjQtMTEtMDciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..PbSFRsSH0LhIkZdcgtpn9Q.RTW1xXnlrViPlU1NFWGrXtlQrHupN-eHGvfNIsVBpU8l2kHV8wiqPliKce-HKvxlOIertDmJ87GHz2LUMxrYBZLRLkU1HSRe7aG8xaTumA0oGdSfGWABbk7_t2sP7VzjBf5sRm-isZwVTixT05poHFw1Y4L_w9bnMKExcD1OE5dXg-xgJMTzqChyLaTcWT1Z3PxZzz0V_xapAakJEdDoYLt0UFr9qfmnfAY8a-5S7CV-6yDhXLZplvixnBAyaxBPvXyB7mzzZpPkNWp4nPcFBiI8F7mUBjcL7GJ_MyBQ5qEu4Me80gutoTHfQ0tL_R0i06BTwIuxPflJ9fQ1wnEcdkGFs66xFmzGEWA4dz9WB1U.ANYk0XXT7n_LUxr0kJ9spIuZ5itMcrHLqQIv76pL6mo"
+// });
 
 const db_id = notion_DB;
 
@@ -42,10 +42,10 @@ exports.getNotion = async function () {
   
 };
 
-exports.getPost = async function (page_id) {
-  const results = await notionAPI.getPage(page_id)
-  console.log("getPost notion api", results);
+// exports.getPost = async function (page_id) {
+//   const results = await notionAPI.getPage(page_id)
+//   console.log("getPost notion api", results);
 
-}
+// }
 
 
