e5062dc01ee6f98648b24ec80c3440f8c36bb497
hotline404 <cosmoyj7733@gmail.com>
2025-08-13 18:11:15 +0900

    faet: auth

--- API/service.js
+++ API/service.js
@@ -7,7 +7,10 @@ const notion = new Client({
   request_timeout: 10000,
 });
 
-const notionAPI = new NotionAPI();
+const notionAPI = new NotionAPI({
+  activeUser: "b8d00329-393d-4390-9f8e-b783e2fe1768",
+  authToken: "v03%3AeyJhbGciOiJkaXIiLCJraWQiOiJwcm9kdWN0aW9uOnRva2VuLXYzOjIwMjQtMTEtMDciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIn0..PbSFRsSH0LhIkZdcgtpn9Q.RTW1xXnlrViPlU1NFWGrXtlQrHupN-eHGvfNIsVBpU8l2kHV8wiqPliKce-HKvxlOIertDmJ87GHz2LUMxrYBZLRLkU1HSRe7aG8xaTumA0oGdSfGWABbk7_t2sP7VzjBf5sRm-isZwVTixT05poHFw1Y4L_w9bnMKExcD1OE5dXg-xgJMTzqChyLaTcWT1Z3PxZzz0V_xapAakJEdDoYLt0UFr9qfmnfAY8a-5S7CV-6yDhXLZplvixnBAyaxBPvXyB7mzzZpPkNWp4nPcFBiI8F7mUBjcL7GJ_MyBQ5qEu4Me80gutoTHfQ0tL_R0i06BTwIuxPflJ9fQ1wnEcdkGFs66xFmzGEWA4dz9WB1U.ANYk0XXT7n_LUxr0kJ9spIuZ5itMcrHLqQIv76pL6mo"
+});
 
 const db_id = notion_DB;
 
