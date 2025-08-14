b3de6a4c9d2696acf0845989d9a9fe8fb01ad508
hotline404 <cosmoyj7733@gmail.com>
2025-08-14 19:49:08 +0900

    fix : add use client and useRouter in default page modal

--- app/@modal/default.jsx
+++ app/@modal/default.jsx
@@ -1,6 +1,9 @@
+"use client"
 import React from "react";
+import { useRouter } from "next/router";
 
 function Default() {
+  const router = useRouter();
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-xl">
