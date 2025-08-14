7105597a0d553dbbad05c4a29e808b4684151104
hotline404 <cosmoyj7733@gmail.com>
2025-08-14 19:28:35 +0900

    feat: change modal page font

--- app/@modal/default.jsx
+++ app/@modal/default.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
-
+import React from "react";
 
 function Default() {
-  return null
+  return (
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+      <div className="bg-white p-6 rounded-xl">
+        <h2>C A N   N O T   F O U N D   P O S T   I D   I N   N O T I O N   P A G E . . . </h2>
+        <button
+          onClick={() => {
+            router.back();
+          }}
+          className="mt-4 text-sm text-gray-500 hover:cursor-pointer"
+        >
+          닫기
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Default;
-
--- app/@modal/items/page.jsx
+++ app/@modal/items/page.jsx
@@ -10,9 +10,9 @@ function Post() {
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-xl">
-        <h2>모달</h2>
+        <h2>* P O S T   P A G E *</h2>
         <button onClick={() => {router.back()}} className="mt-4 text-sm text-gray-500 hover:cursor-pointer">
-          닫기
+          C L O S E
         </button>
       </div>
     </div>
