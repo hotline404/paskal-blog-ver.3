c7b06e54db375eeba1d43289811f4fd5f418107c
hotline404 <cosmoyj7733@gmail.com>
2025-08-14 19:57:47 +0900

    fix default page modal

--- app/@modal/default.jsx
+++ app/@modal/default.jsx
@@ -1,26 +1,7 @@
-"use client"
 import React from "react";
-import { useRouter } from "next/router";
-
-export const dynamic = "force-dynamic";
 
 function Default() {
-  const router = useRouter();
-  return (
-    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-xl">
-        <h2>C A N   N O T   F O U N D   P O S T   I D   I N   N O T I O N   P A G E . . . </h2>
-        <button
-          onClick={() => {
-            router.back();
-          }}
-          className="mt-4 text-sm text-gray-500 hover:cursor-pointer"
-        >
-          C L O S S
-        </button>
-      </div>
-    </div>
-  );
+  return null
 }
 
 export default Default;
