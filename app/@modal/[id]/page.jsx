230f32999688c0fa81e59165050d46fc3c5e231b
hotline404 <cosmoyj7733@gmail.com>
2025-08-15 09:21:31 +0900

    feat: data debug

--- app/@modal/[id]/page.jsx
+++ app/@modal/[id]/page.jsx
@@ -4,21 +4,25 @@ import { getPost } from '@/API/service';
 export const dynamic = "force-dynamic";
 
 function Post({ params }) {
-  async function tryGet (id) {
-    try {
-    const res = await getPost(id);
-    console.log("* r e s   i n   p o s t   m o d a l *", res)
-  } catch (error) {
-    console.error("e r r o r   i n   P o s t   m o d a l", error)
-  }
-  }
+  // async function tryGet (id) {
+  //   try {
+  //   const res = await getPost(id);
+  //   console.log("* r e s   i n   p o s t   m o d a l *", res)
+  // } catch (error) {
+  //   console.error("e r r o r   i n   P o s t   m o d a l", error)
+  // }
+  // }
   
-  tryGet(params.id);
+  // tryGet(params.id);
+
+  const res = getPost(params.id).then(data => {return data})
+
 
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-xl">
         <h2>* P O S T   P A G E *</h2>
+        <div>{res}</div>
         <a href='/' className="mt-4 text-sm text-gray-500 hover:cursor-pointer">
           C L O S E
         </a>
