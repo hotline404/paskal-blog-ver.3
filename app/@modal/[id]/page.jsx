285277f3828fe74c28576405d6a9b353b007e2a9
hotline404 <cosmoyj7733@gmail.com>
2025-08-16 19:35:08 +0900

    feat : code tag

--- app/@modal/[id]/page.jsx
+++ app/@modal/[id]/page.jsx
@@ -16,13 +16,14 @@ function Post({ params }) {
   }
   }  
 
-  tryGet(params.id);
+  const result = tryGet(params.id);
 
   
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-xl">
         <h2>* P O S T   P A G E *</h2>
+        <code>{result}</code>
         <BackButton className="mt-4 text-sm text-gray-500 hover:cursor-pointer">
           C L O S E
         </BackButton>
