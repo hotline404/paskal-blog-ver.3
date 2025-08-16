1f50dcdde9a0bb247f1cb8048055008ed2ed4b89
hotline404 <cosmoyj7733@gmail.com>
2025-08-16 21:25:03 +0900

    feat: rendering data

--- app/@modal/[id]/page.jsx
+++ app/@modal/[id]/page.jsx
@@ -23,7 +23,9 @@ function Post({ params }) {
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-xl">
         <h2>* P O S T   P A G E *</h2>
-        <code>{result}</code>
+        <div>
+          
+        </div>
         <BackButton className="mt-4 text-sm text-gray-500 hover:cursor-pointer">
           C L O S E
         </BackButton>
---
+++ components/common/Article.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Article({ data }) {
+  return (
+    <div>
+      {data.map((item) => {
+        if (item.type === 'paragraph') {
+          return (
+            <p>{item.paragraph.rich_text[0]}</p>
+          )
+        } else if (item.type === 'image') {
+          return (
+            <img src={item.image.file[0]} alt='test'></img>
+          )
+        }
+      })}
+    </div>
+  )
+}
+
+export default Article
