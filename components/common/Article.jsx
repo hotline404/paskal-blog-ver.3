cc59988aea63077db5ffe250647ea766e79f3656
hotline404 <cosmoyj7733@gmail.com>
2025-08-17 12:26:37 +0900

    debug Article

--- components/common/Article.jsx
+++ components/common/Article.jsx
@@ -1,24 +1,21 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 function Article({ data }) {
-
-  const items = data.then(items => {return items});
+  const items = data.then((items) => {
+    return items;
+  });
   return (
     <div>
-      {items ? items.map((item) => {
-        if (item.type === 'paragraph') {
-          return (
-            <p>{item.paragraph.rich_text[0]}</p>
-          )
-        } else if (item.type === 'image') {
-          return (
-            <img src={item.image.file[0]} alt='test'></img>
-          )
-        } 
-      } ): <div>Loading...</div>} 
+      {items ? (
+        items.map((item) => {
+          return <div>{item.paragraph.rich_text[0]}</div>;
+        })
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Article;
