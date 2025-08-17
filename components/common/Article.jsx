1462f27709d6fcbf855373634f927d1e7ed6d03f
hotline404 <cosmoyj7733@gmail.com>
2025-08-17 19:34:37 +0900

    debug rich text console.

--- components/common/Article.jsx
+++ components/common/Article.jsx
@@ -13,7 +13,7 @@ function Article({ data }) {
 
   }, [setBlocks]);
 
-  console.log("blocks", blocks[0]?.type);
+  console.log("blocks", blocks[0]?.paragraph.rich_text);
   return (
     <div>
       {blocks ? (
