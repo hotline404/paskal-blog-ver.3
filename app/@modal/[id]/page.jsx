5c14e5de87f026e3142ba57fc916a91fa2c4d9be
hotline404 <cosmoyj7733@gmail.com>
2025-08-24 01:26:20 +0900

    change h 90

--- app/@modal/[id]/page.jsx
+++ app/@modal/[id]/page.jsx
@@ -26,7 +26,7 @@ function Post({ params }) {
     <div className="fixed inset-0 bg-white/50 flex items-center justify-center z-50">
       <div className="bg-[#FAF4E6] p-6 rounded-xl outline-[#31302B] outline-[5px] w-[80%] h-[80%]">
         <h2 className="italic">/* P O S T P A G E */</h2>
-        <div className="h-full">
+        <div className="h-90%">
           <Article data={result}/>
           <BackButton className="italic mt-4 text-sm text-gray-500 hover:cursor-pointer">
             C L O S E
