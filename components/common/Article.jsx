0cb7c3b7498bd36723300ca4914b536c61543af6
hotline404 <cosmoyj7733@gmail.com>
2025-08-24 23:40:32 +0900

    change return

--- components/common/Article.jsx
+++ components/common/Article.jsx
@@ -13,34 +13,40 @@ function Article({ data }) {
 
   return (
     <div className="h-[100%] text-lg overflow-auto">
-      {blocks !== null ? (
+      {!blocks ? (
+        <div>Loading...</div>
+      ) : (
         blocks?.map((item) => {
           switch (item.type) {
-            case "heading_2": 
+            case "heading_2":
               return (
-                <h2 className="italic font-bold">{item.heading_2.rich_text[0].text.content}</h2>
-              )
-            case "numbered_list_item":
-              return (
-                <p>
-                  {item.numbered_list_item.rich_text[0].text.content}
-                </p>
+                <h2 className="italic font-bold">
+                  {item.heading_2.rich_text[0].text.content}
+                </h2>
               );
+            case "numbered_list_item":
+              return <p>{item.numbered_list_item.rich_text[0].text.content}</p>;
 
             case "image":
-              return <img className="h-80 w-100 object-cover" src={item.image.file.url} alt="포스트 이미지" />;
+              return (
+                <img
+                  className="h-80 w-100 object-cover"
+                  src={item.image.file.url}
+                  alt="포스트 이미지"
+                />
+              );
             default:
               return (
                 <p>
-                  {item.paragraph.rich_text.length === 0
-                    ? <br></br>
-                    : item.paragraph.rich_text[0].text.content}
+                  {item.paragraph.rich_text.length === 0 ? (
+                    <br></br>
+                  ) : (
+                    item.paragraph.rich_text[0].text.content
+                  )}
                 </p>
               );
           }
         })
-      ) : (
-        <div>Loading...</div>
       )}
     </div>
   );
