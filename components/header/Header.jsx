cec6a4ca98360ec3a299c310872346773f03fd64
hotline404 <cosmoyj7733@gmail.com>
2025-08-28 02:05:56 +0900

    fix change function position

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -5,21 +5,20 @@ import Input from "../common/Input";
 import Provider from "@/store/Provider";
 import { getSearchQuery } from "@/API/service";
 
+const handleSubmit = async () => {
+  "use server";
+  const res = await getSearchQuery("fuck");
+  console.log("handle submit test", res);
+};
 const Header = () => {
   //input component 하위에서 캡슐화시켜야 함
   //비즈니스 컴포넌트 분리
-  const handleSubmit = async () => {
-    'use server'
-    const res = await getSearchQuery("fuck");
-    console.log("handle submit test", res);
-    
-  };
   return (
     <div className={mixtureStyle.header}>
       <TitleBox>* P a s k a l *</TitleBox>
       <SearchBox>
         <Provider />
-        <form action={handleSubmit} >
+        <form action={handleSubmit}>
           <Input
             input={{
               name: "검색",
