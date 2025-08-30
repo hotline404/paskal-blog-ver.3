778e0d8285410856e3629d73eacb0a0aa3d6b608
hotline404 <cosmoyj7733@gmail.com>
2025-08-31 06:52:32 +0900

    del items center

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='flex italic font-bold text-orange-500 bg-black justify-center items-center text-7xl h-screen w-full'>
+    <div className='flex italic font-bold text-orange-500 bg-black justify-center text-7xl h-screen w-full'>
       404 not found
     </div>
   )
