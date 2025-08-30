77181de4afc9f8c57789e9422b180a6d365e9c6f
hotline404 <cosmoyj7733@gmail.com>
2025-08-30 21:24:55 +0900

    feat: style code box

--- components/box/CodeBox.jsx
+++ components/box/CodeBox.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function CodeBox(props) {
   return (
-    <div className="bg-gray-400 text-orange-500 clear-left px-0.5 rounded-[6px] w-[50px]">
-      <p className='italic m-2 tracking-wider'>{props.code}</p>
+    <div>
+      <p className='italic m-2 tracking-wider bg-gray-400 text-orange-500 clear-left px-0.5 rounded-[6px]'>{props.code}</p>
     </div>
   )
 }
