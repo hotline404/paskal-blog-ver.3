0c8c3581fd4efda1bc4253019c5f376cb8554c44
hotline404 <cosmoyj7733@gmail.com>
2025-08-30 21:29:03 +0900

    change bg code box

--- components/box/CodeBox.jsx
+++ components/box/CodeBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function CodeBox(props) {
   return (
-    <div className="bg-gray-400 text-orange-500 clear-left px-0.5 rounded-[6px] w-[50%]">
+    <div className="bg-gray-600 text-orange-500 clear-left px-0.5 rounded-[6px] w-[50%]">
       <p className='italic m-2 tracking-wider'>{props.code}</p>
     </div>
   )
