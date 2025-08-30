705cd58c6aaa7fd1f80630b791710322e9877383
hotline404 <cosmoyj7733@gmail.com>
2025-08-30 21:12:27 +0900

    fix margin

--- components/box/CodeBox.jsx
+++ components/box/CodeBox.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function CodeBox(props) {
   return (
     <div className="bg-cyan-300 clear-left w-[80%] px-0.5 rounded-2xl">
-      <p className='italic mx-1'>{props.code}</p>
+      <p className='italic mx-2'>{props.code}</p>
     </div>
   )
 }
