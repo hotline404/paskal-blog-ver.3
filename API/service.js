79ff3088212f9e564eb7fa2be8604d72dec44f0c
hotline404 <cosmoyj7733@gmail.com>
2025-08-30 17:23:04 +0900

    debug fix

--- API/service.js
+++ API/service.js
@@ -49,7 +49,7 @@ exports.getPost = async function (page_id) {
     page_size: 70,
   });
 
-  console.log("results in getPost notion api", results.item.code);
+  console.log("results in getPost notion api", results.code.rich_text[0].text.content);
 
   return results;
 };
