fd3cbbd1e74661e4003c17c6b0be6f08ad42ebfe
hotline404 <cosmoyj7733@gmail.com>
2025-08-31 06:43:53 +0900

    feat : not found page w h

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='italic font-bold text-orange-500 bg-black justify-center items-center text-7xl'>
+    <div className='italic font-bold text-orange-500 bg-black justify-center items-center text-7xl h-full w-full'>
       404 not found
     </div>
   )
