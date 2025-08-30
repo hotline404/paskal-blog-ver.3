aaf91ede71526f3443613fe74f881279d21e72df
hotline404 <cosmoyj7733@gmail.com>
2025-08-30 21:05:56 +0900

    feat style code box

--- components/box/CodeBox.jsx
+++ components/box/CodeBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function CodeBox(props) {
   return (
-    <div className="bg-cyan-300 clear-left w-[100%]">
+    <div className="bg-cyan-300 clear-left w-[80%] p-0.5">
       <p>{props.code}</p>
     </div>
   )
