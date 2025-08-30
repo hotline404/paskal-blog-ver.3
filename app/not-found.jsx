2ef9e305305f0ec047a595ae9f231c702720a144
hotline404 <cosmoyj7733@gmail.com>
2025-08-31 06:47:01 +0900

    fix text not found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='italic font-bold text-orange-500 bg-black justify-center items-center text-7xl h-full w-full'>
+    <div className='flex italic font-bold text-orange-500 bg-black justify-center items-center text-7xl h-[100%] w-full'>
       404 not found
     </div>
   )
