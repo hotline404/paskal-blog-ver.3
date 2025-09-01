fcdc5ffa881fb39f27baa9c88579b58be5994d60
hotline404 <cosmoyj7733@gmail.com>
2025-09-02 07:44:38 +0900

    tkqkf234334

--- components/info/SkillInfo.jsx
+++ components/info/SkillInfo.jsx
@@ -1,13 +1,12 @@
 "use client";
-import React, {useState, useEffect} from "react";
-
+import React, { useState, useEffect } from "react";
 
 function SkillInfo({ data, test }) {
-  const [state, setState] = useState(null)
-  data.then((e) => setState(e))
+  const [state, setState] = useState(null);
   useEffect(() => {
-  console.log("???", state);
-  }, [setState])
+    data.then((e) => setState(e));
+    console.log("???", state);
+  }, [setState]);
 
   return (
     <div>
@@ -18,7 +17,8 @@ function SkillInfo({ data, test }) {
             alt={`${item} 로고`}
           />
         );
-      })} */}asd
+      })} */}
+      asd
     </div>
   );
 }
