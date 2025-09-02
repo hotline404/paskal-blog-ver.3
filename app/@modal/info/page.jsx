757982a1552c8a441d2aa46dd87066eef7fa1b22
hotline404 <cosmoyj7733@gmail.com>
2025-09-02 10:58:30 +0900

    feat : h === w

--- app/@modal/info/page.jsx
+++ app/@modal/info/page.jsx
@@ -47,7 +47,7 @@ function Info() {
     <div className="fixed inset-0 bg-black/50 flex-col items-center justify-center z-50">
       <div className="flex justify-center mt-16 text-yellow-400">
         <img
-          className="h-40 w-50 object-contain rounded-full"
+          className="h-50 w-50 object-contain rounded-full"
           src="/my-notion-face-portrait.png"
           alt="me-image"
         />
